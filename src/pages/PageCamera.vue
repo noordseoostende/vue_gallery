42e3ed5b113b609666656b6bc17744de5a93e2c8
noordseoostende <voropayev1@mail.ru>
2021-03-07 19:29:15 +0100

    video capture

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,14 +1,137 @@
-vue<template>
-  <q-page class="flex flex-center">
-    <h5>
-      Dagon
-    </h5>
-    
+<template>
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <video
+        v-show="!imageCaptured"
+        ref="video"
+        class="full-width"
+        autoplay
+      />
+      <canvas 
+        v-show="imageCaptured"
+        ref="canvas"
+        class="full-width"
+        height="240"
+      />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn
+        v-if="hasCameraSupport"
+        @click="captureImage"
+        color="grey-10"
+        icon="eva-film-outline"
+        size="lg"
+        glossy
+      /><!-- round -->
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="post.caption"
+          class="col col-sm-6"
+          label="Caption"
+          outlined
+          dense
+        />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="post.location"
+          class="col col-sm-6"
+          label="Location"
+          outlined
+          dense
+        >
+        <template v-slot:append>
+          <q-btn
+            icon="eva-navigation-outline"
+            dense
+            flat
+            round
+          />
+        </template>
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+        <q-btn
+          color="cyan-9"
+          label="Post Image"
+          push
+        />
+      </div>
+    </div>
 
   </q-page>
 </template>
 <script>
+import { uid } from 'quasar'
+require('md-gum-polyfill')
 export default {
-  name: 'PageCamera'
+  name: 'PageCamera',
+  data() {
+    return {
+      post: {
+        id: uid(),
+        caption: '',
+        location: '',
+        photo: null,
+        date: Date.now()
+      },
+      imageCaptured: false,
+      hasCameraSupport: true
+    }
+  },
+  methods: {
+    initCamera() {
+      navigator.mediaDevices.getUserMedia({
+        video: true
+      }).then(stream => {
+        this.$refs.video.srcObject = stream
+      }).catch(error => {
+        this.hasCameraSupport = false
+      })
+    },
+    captureImage() {
+      let video = this.$refs.video
+      let canvas = this.$refs.canvas
+      canvas.width = video.getBoundingClientRect().width
+      canvas.height = video.getBoundingClientRect().height
+      let context = canvas.getContext('2d')
+      context.drawImage(video, 0, 0, canvas.width, canvas.height)
+      this.imageCaptured = true
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL())
+    },
+    dataURItoBlob(dataURI) {
+  // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  var byteString = atob(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+
+  // create a view into the buffer
+  var ia = new Uint8Array(ab);
+
+  // set the bytes of the buffer to the correct values
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  var blob = new Blob([ab], {type: mimeString});
+  return blob;
+
+}
+  },
+  mounted() {
+    this.initCamera()
+  }
 }
 </script>
+
+<style lang="sass">
+  .camera-frame
+    border: 2px solid $grey-10
+    border-radius: 10px 
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,15 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card class="card-post" flat bordered>
+
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+      v-for="post in posts"
+      :key="post.id"
+      class="card-post q-mb-md"
+      bordered
+      flat
+    >
       <q-item>
         <q-item-section avatar>
           <q-avatar>
@@ -12,7 +21,7 @@
           <q-item-label
             class="text-bold" >Marco Polo</q-item-label>
           <q-item-label caption>
-            Italie, Rome
+            {{ post.location }}
           </q-item-label>
         </q-item-section>
       </q-item>
@@ -20,20 +29,85 @@
       <q-separator />
 
       <q-img
-        src="https://cdn.quasar.dev/img/parallax2.jpg"
+        :src="post.imageUrl"
       />
       <q-card-section>
-        <div>Gouden Brug</div>
-        <div class="text-caption text-grey">Juni 12 10:30AM</div>
+        <div>{{ post.caption }}</div>
+        <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
       </q-card-section>
     </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+        <q-item-section avatar>
+          <q-avatar size="48px">
+            <img src="https://i.pinimg.com/originals/e4/4a/33/e44a333e6f9f5ace4e62c2733ad704fc.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label
+            class="text-bold" >Marco_Polo</q-item-label>
+          <q-item-label caption>
+            Marco Polo
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+      </div>
+    </div>
+    
     
 
   </q-page>
 </template>
 <script>
+import { date } from 'quasar'
+
 export default {
-  name: 'PageHome'
+  name: 'PageHome',
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: 'Gouden Brug',
+          date: 1615118834244,
+          location: 'Italie, Rome',
+          imageUrl: 'https://i.pinimg.com/236x/00/b0/58/00b058d7a8d5212bb4b4b526f922f814.jpg'
+
+        },
+        {
+          id: 2,
+          caption: 'Gouden Brug',
+          date: 1615118834244,
+          location: 'Italie, Rome',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+
+        },
+        {
+          id: 3,
+          caption: 'Gouden Brug',
+          date: 1615118834244,
+          location: 'Italie, Rome',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+
+        },
+        {
+          id: 4,
+          caption: 'Gouden Brug',
+          date: 1615118834244,
+          location: 'Italie, Rome',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+
+        }
+      ]
+    }
+  },
+  filters: {
+    niceDate(value) {
+      return date.formatDate(value, 'MMMM D h:mmA')
+    }
+  }
 }
 </script>
 
